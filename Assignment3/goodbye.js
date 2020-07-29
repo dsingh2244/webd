625ed678a256acfbe6d598879775451ffbf82dd5
Digvijay singh <dsinght4@gmail.com>
2020-07-29 16:31:45 +0530

    nan

--- Assignment3/goodbye.js
+++ Assignment3/goodbye.js
@@ -10,15 +10,14 @@
 // See Lecture 52, part 1
 (function (window){
   var byeSpeaker = {};
-  byeSpeaker.name = nam;
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
  var speakWord = "Good Bye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-byespeaker.speak=function (name) {
-  console.log(speakWord + " " + byeSpeaker.name);
+byeSpeaker.speak=function (name) {
+  console.log(speakWord + " " + name);
 }
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
--- Assignment3/hello.js
+++ Assignment3/hello.js
@@ -7,16 +7,14 @@
 // See Lecture 52, part 1
 (function (window){
   var helloSpeaker = {};
-  helloSpeaker.name = nam;
-
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-var speakWord = "Hello";
+  var speakWord = "Hello";
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-hellospeaker.speak = function (name) {
-  console.log(speakWord + " " + helloSpeaker.name);
+helloSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
 }
 
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
